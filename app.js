b15045e52200cb132441bb70aa07b7aa3e482534
Avinash Chodavarapu <60456166+avinashtechlvr@users.noreply.github.com>
2021-08-27 00:53:17 +0530

    Added Ui and added task adding facility

---
+++ app.js
@@ -0,0 +1,52 @@
+// Define UI Vars
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+
+function loadEventListeners(){
+    form.addEventListener('submit',addTask);
+}
+
+//Add Task
+function addTask(e){
+    if(taskInput.value == ''){
+        alert('Add a task');
+    }
+    // create li element
+    const li = document.createElement('li');
+    // Adding Class
+    li.className = 'collection-item';
+
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // Create new link element
+    const link = document.createElement('a');
+
+    // Adding Class
+    link.className = 'delete-item secondary-content';
+
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+
+    // Append link to li
+    li.appendChild(link);
+
+    // Append li to ul
+
+    taskList.appendChild(li)
+
+    // Clear input
+    taskInput.value='';
+
+    e.preventDefault();
+
+}
